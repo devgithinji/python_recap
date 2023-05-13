b4dc82be32a47f54381f384640b3cc461f00ab0c
dennis githinji <wakahiad@gmail.com>
2023-05-13 14:41:31 +0300

    using json library

--- assignment6.py
+++ assignment6.py
@@ -1,4 +1,7 @@
+import json
+
 running = True
+user_input_list = []
 while running:
     print('Please choose')
     print('1: Add Input')
@@ -7,16 +10,14 @@ while running:
     user_input = input('Your choice: ')
     if user_input == '1':
         data_to_store = input('Your text: ')
-        with open('assignment.txt', mode='a') as f:
-            f.write(data_to_store)
+        user_input_list.append(data_to_store)
+        with open('assignment.txt', mode='w') as f:
+            f.write(json.dumps(user_input_list))
             f.write("\n")
     elif user_input == '2':
         with open('assignment.txt', mode='r') as f:
-            file_content = f.readlines()
+            file_content =json.loads(f.read())
             for line in file_content:
                 print(line)
     elif user_input == 'q':
         running = False
-
-
-
